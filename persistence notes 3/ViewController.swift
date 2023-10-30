cb944658f3da361ab440eb7de7d8e35810e3bccd
Oliver Murray <creeperman69@icloud.com>
2023-10-30 10:00:18 -0500

    Initial Commit

---
+++ persistence notes 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  persistence notes 3
+//
+//  Created by OLIVER MURRAY on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ persistence notes 3UITests/persistence_notes_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  persistence_notes_3UITestsLaunchTests.swift
+//  persistence notes 3UITests
+//
+//  Created by OLIVER MURRAY on 10/30/23.
+//
+
+import XCTest
+
+final class persistence_notes_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
